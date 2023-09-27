145584665c8ab63629be58f7094db696ee81de73
me <myemail@example.com>
2023-09-28 08:52:23 +0900

    カレンダー問題リファクタ

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -17,17 +17,15 @@ OptionParser.new do |opts|
 end.parse!
 month = options[:month] || today.month
 
-unless year.include?(month)
-  raise "#{month} is neither a month number (1..12) nor a name"
-else
-  puts "      #{month}月 2023"
-  days = [*Date.new(2023, month, 1).day..Date.new(2023, month, -1).day]
-  week
-  cwday = Date.new(2023, month, 1).cwday
-  first_week = days.slice(0..(7 - cwday)).map { |day| day.to_s.rjust(2, ' ') }.join(' ')
-  puts '   ' * (cwday - 1) + first_week
-  other_week = days.slice((7 - cwday + 1)..-1)
-  other_week.each_slice(7) do |a|
-    puts a.map { |b| b.to_s.rjust(2, ' ') }.join(' ')
-  end
+raise "#{month} is neither a month number (1..12) nor a name" unless year.include?(month)
+
+puts "      #{month}月 2023"
+days = [*Date.new(2023, month, 1).day..Date.new(2023, month, -1).day]
+week
+cwday = Date.new(2023, month, 1).cwday
+first_week = days.slice(0..(7 - cwday)).map { |day| day.to_s.rjust(2, ' ') }.join(' ')
+puts '   ' * (cwday - 1) + first_week
+other_week = days.slice((7 - cwday + 1)..-1)
+other_week.each_slice(7) do |a|
+  puts a.map { |b| b.to_s.rjust(2, ' ') }.join(' ')
 end
