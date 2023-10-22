fe8d5c0b199562c4264bb630e2bc95abb2c03da1
me <myemail@example.com>
2023-10-22 19:00:18 +0900

    Suica版自動販売機問題提出

--- ruby/vending_machine/main.rb
+++ ruby/vending_machine/main.rb
@@ -14,101 +14,93 @@ end
 
 
 class Suica
-  # 預り金を定義する
   DEPOSIT = 500
+
   def initialize
     @balance = DEPOSIT
   end
-  # チャージ
+
   def charge(money)
     if money < 100
       raise "100円未満はチャージできません"
     else
       @balance = DEPOSIT + money
-      puts "残高は#{@balance}円です"
     end
   end
 
-  private
-  # 残高取得
+  # private
   def balance
-    # puts "残高は#{@balance}円です"
     @balance
   end
 
-  
+  def balance=(new_balance)
+    @balance = new_balance
+  end
 end
 
 
 class VendingMachine
   def initialize(juice, stock_num)
-    # 売上金を定義する
+    @purchasable_juice = []
     @sales = 0
-    # 在庫を定義する
     @stocks = { juice.name => stock_num }
   end
 
-  # ジュース追加
   def add_item=(new_juice)
-    # @stocks = {juice.name => num}
-    # @stocks[juice.name] = num
-    # puts "#{juice.name}の在庫は#{@stocks[juice.name]}本です"
     @stocks[new_juice[0]] = new_juice[1]
 
   end
-  # 在庫補充
-  # def add_stock(juice, a)
-  #   @stocks[juice.name] += a
-  #   puts @stocks[juice.name]
-  # end
+
+  def add_stock(juice, a)
+    @stocks[juice.name] += a
+  end
 
   def show_stock
-    puts @stocks
+    @stocks
   end
-  # 支払処理
-  # どのジュースのボタンが押されたか感知する
-  # Suicaの支払い処理
-  def purchase(card, juice)
-    if card.balance >= juice.price && @stocks[juice.name] > 0
-      # puts "#{juice.name}は買えます"
+
+  def purchase(card, juice, num)
+    if card.balance >= juice.price * num && @stocks[juice.name] >= num
+      card.balance -= juice.price * num
       @sales += juice.price
-      # puts "売上金が#{@sales}円になりました"
-      @stocks[juice.name] -= 1
-      # puts "#{juice.name}は残り#{@stocks[juice.name]}本です"
-    else card.balance < juice.price || @stocks[juice.name] == 0
-      puts "#{juice.name}は買えません"
+      @stocks[juice.name] -= num
+    else card.balance < juice.price || @stocks[juice.name] < num
+      raise "Suicaの残高が足りないか、在庫の数が足りないため#{juice.name}は買えません"
     end
   end
 
 
   def purchasable_juice(card, juice)
-    if card.balance >= juice.price && @stocks[juice.name]
-      puts "#{juice.name}は買えます"
+    if card.balance >= juice.price && @stocks[juice.name] > 0
+      @purchasable_juice.push(juice.name)
     end
+  end
+
+  def show_purchasable_juice
+    @purchasable_juice
   end
 
   private
   def sales
-    # p @sales
-    puts "売上金は#{@sales}円です"
+    @sales
   end
 end
 
-pepsi = Juice.new("ペプシ", 150)
-suica = Suica.new
-suica.charge(100)
-# suica.balance
-vendingmachine = VendingMachine.new(pepsi, 5)
-# vendingmachine.stock(pepsi, 5)
-vendingmachine.purchase(suica, pepsi)
-# vendingmachine.add_stock(pepsi, 2)
-# vendingmachine.sales
-monster = Juice.new("モンスター", 230)
-irohasu = Juice.new("いろはす", 120)
-# vendingmachine.stock(monster, 5)
-vendingmachine.add_item = [monster.name, 5]
-# vendingmachine.add_item(irohasu, 5)
-vendingmachine.show_stock
+# pepsi = Juice.new("ペプシ", 150)
+# suica = Suica.new
+# suica.charge(100)
+# # suica.balance
+# vendingmachine = VendingMachine.new(pepsi, 5)
+# # vendingmachine.stock(pepsi, 5)
+# vendingmachine.purchase(suica, pepsi)
+# # vendingmachine.add_stock(pepsi, 2)
+# # vendingmachine.sales
+# monster = Juice.new("モンスター", 230)
+# irohasu = Juice.new("いろはす", 120)
+# # vendingmachine.stock(monster, 5)
+# vendingmachine.add_item = [monster.name, 5]
+# # vendingmachine.add_item(irohasu, 5)
+# vendingmachine.show_stock
 # vendingmachine.add_stock(monster, 3)
-vendingmachine.purchasable_juice(suica, pepsi)
-# vendingmachine.add_stock(monster, 5)
+# vendingmachine.purchasable_juice(suica, pepsi)
+# # vendingmachine.add_stock(monster, 5)
