9c747703aa2c562233c57f2ae45477b563a1b2cf
me <myemail@example.com>
2023-11-03 21:56:39 +0900

    Suica版自動販売機問題リファクタ(Rubocop通した)

--- ruby/vending_machine/suica.rb
+++ ruby/vending_machine/suica.rb
@@ -10,12 +10,14 @@ class Suica
 
   def charge(money)
     raise '100円未満はチャージできません' if money < 100
+
     @balance = DEPOSIT + money
   end
 
   def purchase(vendingmachine, card, juice)
     raise "Suicaの残高が足りないため#{juice.name}は買えません" if card.balance < juice.price
     raise "在庫の数が足りないため#{juice.name}は買えません" if vendingmachine.stocks[juice] == 0
+
     @balance -= juice.price
     vendingmachine.stocks[juice] -= 1
   end
